003e761516e9b1889ef45dd21da7c1d325a905c5
akashsaini3032 <akashsainira@gmail.com>
2025-01-14 23:05:27 +0530

    update content

--- array/klargest.cpp
+++ array/klargest.cpp
@@ -4,16 +4,22 @@ using namespace std;
 //Find K largest in the given array
 
 int main() {
-    int arr[]={2,1,6,3,7};
+    int arr[]={1,2,5,6,9,4};
     int num;
     
-    for(int i=0;i<5;i++){
-        for(int j=i+1;j<5;j++)
-        if(arr[i]<arr[j]){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-        }
+    for(int i=0;i<6;i++){
+
+           for(int j=i+1;j<6;j++){
+            if(arr[i]<arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                 arr[j]=temp;
+            }
+            
+        
+           }
+
+        
     }
 
     cout<<"Enter the number:";
--- array/targetsum.cpp
+++ array/targetsum.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 int main() {
     int arr[]={2,3,1,4,7};
-    int sum;
+    int sum=0;
     
-    for(int i=0;i<5;i++){
-        for(int j=i+1;j<5;j++){
-            if(arr[i]==arr[j]){
-            arr[j]=-1;
+    for(int i=0;i<=4;i++){
+        for(int j=i+1;j<=4;j++){
+
+            sum=arr[i]+arr[j];
+            if(sum==6)
+            {
+                cout<<arr[i]<<arr[j];
+            }
 
         }
-        }
+        
         
     }
 
