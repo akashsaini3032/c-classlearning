a4c1c5351a7be4daca27d305a5ad8096bb22af9a
akashsaini3032 <akashsainira@gmail.com>
2025-03-30 22:35:29 +0530

    update content

--- Oops/Construtorexplain.cpp
+++
@@ -1,9 +0,0 @@
-
-//MOST IMPORTANT INFORMATION OF CONSTRUCTORS
-
-constructor=to initialize variable of object;
-defination=it is a special function
-why it is special?
-because its name is same as class name.
-it has no return type
-it calls at the time of creation of object automatically
--- Oops/concept6explain.cpp
+++
@@ -1,7 +0,0 @@
-//in concdept 6 explanation is
-//agar two object same adress share karte hai and first object hi na raha to usk object bhi nahi rahega than iska matlab second object bhi 
-//garbage value lene lagega 
-///yah copy constructor se related hai
-//
-//
-//
