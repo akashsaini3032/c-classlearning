3f517827c29132b55227fa937a0f616c279551b6
akashsaini3032 <akashsainira@gmail.com>
2025-04-08 22:55:41 +0530

    practice array

--- array/array1.cpp
+++ array/array1.cpp
@@ -15,19 +15,3 @@ int main() {
 }
 
 
-// copy
-#include <iostream>
-using namespace std;
-
-int main() {
-    int arr[]={101,102,103,104,105 }
-    
-    cout<<arr[0];
-    cout<<arr[1];
-    cout<<arr[2];
-    
-    for(int i=0;i<=2;i++)
-    {
-        cout<<arr[i];
-    }
-}
--- array/array3.cpp
+++ array/array3.cpp
@@ -20,3 +20,30 @@ int main() {
     
    
 }
+
+
+
+
+//copy
+#include <iostream>
+using namespace std;
+
+// input and output
+
+int main() {
+    int arr[5];
+    for(int i=0;i<=4;i++){
+        
+        cin>>arr[i];
+    }
+    
+    for(int i=0;i<=4;i++) {
+        
+        cout<<arr[i]+5
+        ;
+    }
+    
+    
+    
+   
+}
