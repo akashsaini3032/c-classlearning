5300b4d3b3646d2f59f1f94d77963af59e849de1
akashsaini3032 <akashsainira@gmail.com>
2025-04-10 23:31:23 +0530

    update content

--- array/array4.cpp
+++ array/array4.cpp
@@ -1,23 +1,23 @@
-// #include <iostream>
-// using namespace std;
-// //add+5
+#include <iostream>
+using namespace std;
+//add+5
 
 
 
 
-// int main() {
-//     int arr[5];
-//     for(int i=0;i<=4;i++){
+int main() {
+    int arr[5];
+    for(int i=0;i<=4;i++){
         
-//         cin>>arr[i];
-//     }
+        cin>>arr[i];
+    }
     
-//     for(int i=0;i<=4;i++) {
-//         arr[i]=arr[i]+5;
-//         cout<<arr[i];
-//     }
+    for(int i=0;i<=4;i++) {
+        arr[i]=arr[i]+5;
+        cout<<arr[i];
+    }
     
     
     
    
-// }
+}
