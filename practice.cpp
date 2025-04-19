ecbbca62dabc7a7197d255f71fe4bdfa271bf8d9
akashsaini3032 <akashsainira@gmail.com>
2025-04-19 22:43:16 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -32,21 +32,3 @@ int main()
 
 
 
-//third
-
-#include <iostream>
-using namespace std;
-class a
-{
-    int g;
-    public: a(int x)
-    {
-        g=x;
-        cout<<g;
-    }
-};
-int main()
-{
-    a obj(10);
-    a obj1;
-}
