2921763f58ef5c8b6a8e5aa3ed307f4e8b5438dc
akashsaini3032 <akashsainira@gmail.com>
2025-04-19 22:44:20 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -1,34 +1,3 @@
-// first 
-
-#include <iostream>
-class a
-{
-    int g;
-    public: a(int x)
-    {
-        g=x;
-    }
-};
-int main()
-{
-    a obj(10);
-}
-
-
-#include <iostream>
-using namespace std;
-class a
-{
-    int g;
-    public: a()
-    {
-        cout<<"i am constructor";
-    }
-};
-int main()
-{
-    a obj;
-}
 
 
 
