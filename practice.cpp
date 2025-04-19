6f55d65def1018dd719d433f44fa7121eb613994
akashsaini3032 <akashsainira@gmail.com>
2025-04-19 22:46:04 +0530

    constructor practice

--- practice.cpp
+++ practice.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+using namespace std;
+//Shallow copy
+class one{
+    int a;
+    public:one(int x)
+    {
+    
+        a=x;
+        cout<<a;
+    }
+    public:one(one &obj)
+    {
+        
+        a=obj.a;
+        cout<<a;
+    }
+};
+
+int main()
+{
+    one obj(10);
+    one obj1(obj);
+}
 
 
 
