48ac480601514c6722616c0529f0b9519794349f
akashsaini3032 <akashsainira@gmail.com>
2025-04-21 22:51:34 +0530

    update content

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,53 @@
 
+////7 Reversing a Number :
+
+#include <iostream>
+
+using namespace std;
+
+int reverseNumber(int num) {
+    int rev = 0;
+    while (num != 0) {
+        rev = rev * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    int rev = reverseNumber(num);
+    cout << "Reversed Number: " << rev;
+    return 0;
+}
+
+
+//8ARMSTRONG NUMBER :
+#include <iostream>
+
+using namespace std;
+
+bool isArmstrongNumber(int num) {
+    int sum = 0;
+    int cpy = num;
+    while (num != 0) {
+        int digit = num % 10;
+        sum += digit * digit * digit;
+        num = num / 10;
+    }
+    return cpy == sum;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isArmstrongNumber(num)) {
+        cout << "Armstrong";
+    } else {
+        cout << "No";
+    }
+    return 0;
+}
