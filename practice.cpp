77345c8371d6769ead05c6b61587d49034e81aef
akashsaini3032 <akashsainira@gmail.com>
2025-05-07 23:47:09 +0530

    update content

---
+++ practice.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// after concept 3 image see then see this code
+//hybrid inherintance
+class A
+{
+    public:
+         int p=10;
+         A()
+         {
+             cout<<"A";
+         }
+};
+class B: virtual public A
+{
+    public: B()
+    {
+        cout<<"B";
+    }
+};
+class C: virtual public A
+{
+    public: C()
+    {
+        cout<<"C";
+    }
+};
+
+class D:public C, public B
+{
+    public: D()
+    {
+        cout<<"D";
+    }
+};
+
+
+
+int main()
+{
+    D obj;
+
+    
+}
