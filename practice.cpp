8cb3c94a03fa6398a2f33c92a2356d02c71826e9
akashsaini3032 <akashsainira@gmail.com>
2025-05-22 00:16:27 +0530

    update files

--- practice.cpp
+++ practice.cpp
@@ -4,32 +4,3 @@
 
 
 
-#include <iostream>
-using namespace std;
-int main() {
-    int ft=0;
-    int st=1;
-    int next=0;
-    for(int i=0;i<5;++i)
-    {
-        if(i==0)
-        {
-            cout<<ft<<endl;
-            continue;
-        }
-        
-        if(i==1)
-        {
-        cout<<st<<endl;
-        continue;
-        }
-    
-
-    next=ft+st;
-    ft=st;
-    st=next;
-    
-    cout<<next<<endl;
-} 
-}
-    
