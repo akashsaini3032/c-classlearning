c6f3e2db93d4c893d54ef26aaa264b4d9f9d3eec
akashsaini3032 <akashsainira@gmail.com>
2025-05-23 23:16:01 +0530

    reverse string

--- practice.cpp
+++ practice.cpp
@@ -6,29 +6,24 @@
 
 #include <iostream>
 using namespace std;
-
-//uc uppercase lc lower case
-void uclc(string &str1)
+void rev(string &str)
 {
-    for(int i=0; i<str1.length(); ++i)
+    int start=0;
+    int end=str.length()-1;
+    while(start<end)
     {
-        if(str1[i]>='A' && str1[i]<='Z')
-        {
-            str1[i] = str1[i] + 32;
-        }
-        else if(str1[i]>='a' && str1[i]<='z')
-        {
-            str1[i] = str1[i] - 32;
-        }
-        
+        char temp=str[start];
+        str[start]=str[end];
+        str[end]=temp;
+    start++;
+    end--;
     }
 }
 
 int main()
 {
-    string str1 = "AbCd";
-    uclc(str1);
-    cout << str1;
-
-    return 0;
+    string str="akash";
+    rev(str);
+    
+    
 }
