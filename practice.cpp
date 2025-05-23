04f2779bcd90dd75bfb2bb468a7715ea5e211556
akashsaini3032 <akashsainira@gmail.com>
2025-05-23 23:16:34 +0530

    update folders

--- practice.cpp
+++ practice.cpp
@@ -4,26 +4,3 @@
 
 
 
-#include <iostream>
-using namespace std;
-void rev(string &str)
-{
-    int start=0;
-    int end=str.length()-1;
-    while(start<end)
-    {
-        char temp=str[start];
-        str[start]=str[end];
-        str[end]=temp;
-    start++;
-    end--;
-    }
-}
-
-int main()
-{
-    string str="akash";
-    rev(str);
-    
-    
-}
