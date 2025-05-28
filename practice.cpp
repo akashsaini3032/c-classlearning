88397a5f61e3b131a21da1a0bfedbe77318565fc
akashsaini3032 <akashsainira@gmail.com>
2025-05-28 23:08:52 +0530

    n teerm practice

--- practice.cpp
+++ practice.cpp
@@ -4,3 +4,17 @@
 
 
 
+#include <iostream>
+using namespace std;
+int main() {
+    int term;
+    int n=5;
+    
+    for(int i=1;i<=n;++i)
+    {
+    term=term*10+1;
+    cout<<term<<endl;
+    }
+
+}
+    
