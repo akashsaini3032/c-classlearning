1fd08cb3f619dd5448d12336e932f787d67b78bc
akashsaini3032 <akashsainira@gmail.com>
2025-05-28 23:09:21 +0530

    update

--- practice.cpp
+++ practice.cpp
@@ -4,17 +4,4 @@
 
 
 
-#include <iostream>
-using namespace std;
-int main() {
-    int term;
-    int n=5;
-    
-    for(int i=1;i<=n;++i)
-    {
-    term=term*10+1;
-    cout<<term<<endl;
-    }
-
-}
-    
+ 
