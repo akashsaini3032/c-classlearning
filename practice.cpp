6152303fd231994c33f1fd551ed84f0249e4bff9
akashsaini3032 <akashsainira@gmail.com>
2025-05-31 21:48:56 +0530

    reverse string

--- practice.cpp
+++ practice.cpp
@@ -1,33 +1,28 @@
-//12Fibonacci series :
-#include <iostream>
-
-using namespace std;
-
-void printFibonacciSeries(int n) {
-    int ft = 0;
-    int st = 1;
-    for (int i = 0; i < n; ++i) {
-        if (i == 0) {
-            cout << ft << endl;
-            continue;
-        }
-        if (i == 1) {
-            cout << st << endl;
-            continue;
-        }
-        int next = ft + st;
-        ft = st;
-        st = next;
-        cout << next << endl;
-    }
-}
-
-int main() {
-    int n = 5;
-    cout << "Fibonacci Series: " << endl;
-    printFibonacciSeries(n);
-    return 0;
-}
 
     
  
+
+
+ #include <iostream>
+using namespace std;
+void rev(string &str)
+{
+    int start=0;
+    int end=str.length()-1;
+    while(start<end)
+    {
+        char temp=str[start];
+        str[start]=str[end];
+        str[end]=temp;
+    start++;
+    end--;
+    }
+}
+
+int main()
+{
+    string str="akash";
+    rev(str);
+    
+    
+}
