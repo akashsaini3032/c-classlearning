980b1abf657e64a93c549a0600bb0cf9913e6563
akashsaini3032 <akashsainira@gmail.com>
2025-05-31 21:48:00 +0530

    fabonaci practice

--- practice.cpp
+++ practice.cpp
@@ -1,3 +1,33 @@
+//12Fibonacci series :
+#include <iostream>
+
+using namespace std;
+
+void printFibonacciSeries(int n) {
+    int ft = 0;
+    int st = 1;
+    for (int i = 0; i < n; ++i) {
+        if (i == 0) {
+            cout << ft << endl;
+            continue;
+        }
+        if (i == 1) {
+            cout << st << endl;
+            continue;
+        }
+        int next = ft + st;
+        ft = st;
+        st = next;
+        cout << next << endl;
+    }
+}
+
+int main() {
+    int n = 5;
+    cout << "Fibonacci Series: " << endl;
+    printFibonacciSeries(n);
+    return 0;
+}
 
     
  
