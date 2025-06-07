07ce52ed6581ad3fd791885119455413be48fcd4
akashsaini3032 <akashsainira@gmail.com>
2025-06-07 22:55:03 +0530

    update

--- practice.cpp
+++ practice.cpp
@@ -1,31 +1,3 @@
 
     
  
-#include <iostream>
-using namespace std;
-void pallindrome(string &str)
-{
-    int start=0;
-    int end=str.length()-1;
-    while(start<end)
-    {
-        if(str[start]!=str[end])
-    {
-        cout<<"not pallindrome";
-        return;
-    }
-    start++;
-    end--;
-    }
-    cout<<"pallindrome";
-}
-
-int main()
-{
-    string str="madam";
-    pallindrome(str);
-    
-    
-}
-
- 
