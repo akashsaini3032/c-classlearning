aeff3173e9458160694a438d88207bb01a05ee2d
akashsaini3032 <akashsainira@gmail.com>
2025-06-07 22:55:58 +0530

    practice pattern printing

--- practice.cpp
+++ practice.cpp
@@ -1,3 +1,17 @@
 
     
  
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int term=0;
+for (int i = 1; i <=n; i++){
+     
+     term=term*10+1;
+     
+     cout<<term<<endl;
+    }
+
+}
