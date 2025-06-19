ce4ea1ee1f7745d3bc8802e70708e677716c94f0
akashsaini3032 <akashsainira@gmail.com>
2025-06-19 22:56:33 +0530

    DSA first Day

---
+++ DSA-Class/firstDay.cpp
@@ -0,0 +1,41 @@
+// BASICS :
+// #include<iostream>
+// using namespace std;
+// int a=8;
+// int main(){
+//     int a=90;
+//     cout<<a;// This will give variable in local scope
+//     cout<<::a<<endl;//This will give access to global variable & it will be 8.
+// }
+// #include<iostream>
+// using namespace std;
+// int a=8;
+// int main(){
+
+//     cout<<a/0<<endl;
+//     cout<<"CODE RUN SUCCESSFULLY";
+//     return 0;//return 0 means that ur code rull entirely successfully
+// }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int arr[] = {45, 87, 23, 12, 34};
+//   for (int i = 0; i < 5; i++)
+//   {
+//     for (int j = i + 1; j < 5; j++)
+//     {
+//       if (arr[i] > arr[j])
+//       {
+//         int temp;
+//         temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//       }
+//     }
+//   }
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << arr[i] << " ";
+//   }
+// }
